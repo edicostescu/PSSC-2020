f6cef4c46deb74eb76c0f7cd6baea8125eb6ca1a
edicostescu <edww@EdwardsDesktop.localdomain>
2020-11-28 15:47:28 +0200

    PROJECT L08-L13 : QuestionWorkflow

---
+++ EduardCostescu/Project L08-L13/stackunderflow/Samples/StackUnderflow.Core/Contexts/Question/QuestionReadContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using StackUnderflow.EF.Models;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question
+{
+    class QuestionReadContext
+    {
+        public IEnumerable<Post> questions_ { get; }
+
+        public QuestionReadContext(IEnumerable<Post> questions)
+        {
+            questions_ = questions;
+        }
+    }
+}
